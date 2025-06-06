5137bf7747c31ee95a18e7706bcc6ed8789e62b5
Jose <jvrs.90@gmail.com>
2025-06-06 12:32:01 +0200

    03: authentification setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return <Button>HOLA</Button>;
+  const { data: session, isPending, error, refetch } = authClient.useSession();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = async () => {
+    await authClient.signUp.email(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Signed up successfully");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+        <h1>Welcome {session.user.name}</h1>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Input
+        type="text"
+        value={name}
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        type="email"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleSignUp}>Sign Up</Button>
+    </div>
+  );
 }
