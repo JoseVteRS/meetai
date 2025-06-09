c74b23d77818cef1eb4a707f361015b1ae8c0ca2
Jose <jvrs.90@gmail.com>
2025-06-09 17:26:13 +0200

    08 trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,16 +4,13 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if(!session) {
-    redirect("/sign-in")
+  if (!session) {
+    redirect("/sign-in");
   }
 
-  return (
-    <HomeView />
-  );
+  return <HomeView />;
 }
